5ab197c21c8539b227593037cc5de018b1471425
Kirill Klimov <schop1schop@gmail.com>
2021-01-04 01:41:59 +0500

    repo link fixed

--- src/pages/about/about-page.component.jsx
+++ src/pages/about/about-page.component.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
       <h1>Redux-pillows</h1>
       <p>Redux-Pillows is simple e-commerce <b>React</b> app made by <S.Link target="_blank" href='https://github.com/kuhkowaba'>@kuhkowaba</S.Link></p>
       <p>The app uses <b>Redux</b> for global state management. Side effects (Firestore data fetching) were implemented using <b>redux-saga</b>.</p> 
-      <p><S.Link href='#'>Github repository</S.Link></p>
+      <p><S.Link href='https://github.com/kuhkowaba/redux-pillows'>Github repository</S.Link></p>
     </S.Container>
   );
 }
